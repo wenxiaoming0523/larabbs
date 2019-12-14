96197814d091e632da16a62fe13e9b7440496db6
wenxiaoming <wenming@shoufunet.com>
2019-12-14 16:10:28 +0800

    移动 User 模型到 app/models 目录

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -11,7 +11,7 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are mass assignable.U
      *
      * @var array
      */
