da9bc7458da1125c2e2d4237edeb8571b4882f49
wenxiaoming <wenming@shoufunet.com>
2019-12-14 13:53:52 +0800

    修复跳转链接

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@
 Route::get('/','PagesController@root')->name('root');
 
 // 用户身份验证相关的路由
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login'); Route::post('login', 'Auth\LoginController@login');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 // 用户注册相关路由
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
