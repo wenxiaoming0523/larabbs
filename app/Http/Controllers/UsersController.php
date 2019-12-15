d6db7ae312ca5f6cdbc1326ecc09ecddb2833eb5
wenxiaoming <wenming@shoufunet.com>
2019-12-15 22:51:02 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,11 +19,17 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
 
-        //dd($request->all());
-        $user->update($request->all());
+        $data = $request->all();
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料修改成功');
     }
 }
