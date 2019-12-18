7bd9798b9a1357dd4c0ee60cb3b1b3f00624bd55
wenxiaoming <wenming@shoufunet.com>
2019-12-18 11:40:40 +0800

    修复 XSS 注入漏洞

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -2,32 +2,32 @@
 
 @section('content')
 
-@section('styles')
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
-@stop
-@section('scripts')
-  <script type="text/javascript" src="{{asset('jquery.min.js')}}"></script>
-  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
-  <script>
-    $(document).ready(function() {
-      var editor = new Simditor({
-        textarea: $('#editor'),
-        upload: {
-          url: '{{ route('topics.upload_image') }}',
-          params: {
-            _token: '{{ csrf_token() }}'
-          },
-          fileKey: 'upload_file',
-          connectionCount: 1,
-          leaveConfirm: '文件上传中，关闭此页面将取消上传。'
-        },
-    //    pasteImage: true,
-      }); });
-  </script>
-@stop
+{{--@section('styles')--}}
+  {{--<link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">--}}
+{{--@stop--}}
+{{--@section('scripts')--}}
+  {{--<script type="text/javascript" src="{{asset('jquery.min.js')}}"></script>--}}
+  {{--<script type="text/javascript" src="{{ asset('js/module.js') }}"></script>--}}
+  {{--<script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>--}}
+  {{--<script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>--}}
+  {{--<script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>--}}
+  {{--<script>--}}
+    {{--$(document).ready(function() {--}}
+      {{--var editor = new Simditor({--}}
+        {{--textarea: $('#editor'),--}}
+        {{--upload: {--}}
+          {{--url: '{{ route('topics.upload_image') }}',--}}
+          {{--params: {--}}
+            {{--_token: '{{ csrf_token() }}'--}}
+          {{--},--}}
+          {{--fileKey: 'upload_file',--}}
+          {{--connectionCount: 1,--}}
+          {{--leaveConfirm: '文件上传中，关闭此页面将取消上传。'--}}
+        {{--},--}}
+    {{--//    pasteImage: true,--}}
+      {{--}); });--}}
+  {{--</script>--}}
+{{--@stop--}}
 
 <div class="container">
   <div class="col-md-10 offset-md-1">
