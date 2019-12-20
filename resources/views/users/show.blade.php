942fc07c8201d30f45283550b6050397c4bd3bba
wenxiaoming <wenming@shoufunet.com>
2019-12-20 15:49:10 +0800

    用户可以发表评论

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -25,11 +25,25 @@
     <div class="card ">
       <div class="card-body">
         <ul class="nav nav-tabs">
-          <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li> </ul>
-        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
-      </div>
+          <li class="nav-item">
+            {{--<a class="nav-link active bg-transparent" href="#">Ta 的话题--}}
+              <a class="nav-link bg-transparent {{ active_class(if_query('tab', null)) }}" href="{{ route('topics.index') }}">
+                Ta 的话题
+            </a>
+          </li>
+          {{--<li class="nav-item"><a class="nav-link" href="#">--}}
+                <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}" href="{{ route('topics.index') }}">
+            Ta 的回复
+            </a>
+          </li>
+        </ul>
+        @if (if_query('tab', 'replies'))
+          @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+        @else
+          @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+        @endif
     </div>
   </div>
-  </div>
+</div>
+</div>
 @stop
